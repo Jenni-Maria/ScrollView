f1af4e4905808f52f3a43cb0a80f3fd3b7317893
Jenni-Maria <t1kyje00@students.oamk.fi>
2023-09-26 16:05:57 +0300

    ScrollView with extras

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Image,  Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { AppBar } from '@react-native-material/core';
 
 export default function App() {
+  const [items, setItems] = useState([
+    {id: 1, name: 'Test1'},
+    {id:2,name: 'Test2'}
+  ])
+
+  useEffect(() => {
+    const tempItems = [] //taulukon lyhyt määrittely
+    for (let i =1;i<41;i++) {
+      tempItems.push({id: i,name: `Test ${i}`, image: 'https://reactnative.dev/img/tiny_logo.png'})
+    }
+    setItems(tempItems)
+  }, [])
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor='#ccc'/>
+      <AppBar title='My list' backgroundColor='#999' tintColor='cyan'/>
+     <ScrollView>
+      {
+        items.map((item) => (
+          <View key={item.id} style={styles.rowContainer}>
+            <Image
+              source = {{ 
+                uri: item.image,
+                width: 32,
+                height: 32,
+              }}
+            />
+          
+        <Text style={styles.rowText}>{item.name}</Text>
+        </View>
+      ))
+      }
+      </ScrollView> 
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    
     flex: 1,
+    marginLeft: 10,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight: 0,
   },
+  rowContainer: {
+    
+    flexDirection: 'row',
+    marginTop: 4,
+    marginBottom: 4,
+  },
+  rowText: {
+    fontSize: 20,
+    marginTop: 4,
+    marginBottom: 4,
+    marginLeft: 10,
+    color: 'blue',
+    
+  }
 });
